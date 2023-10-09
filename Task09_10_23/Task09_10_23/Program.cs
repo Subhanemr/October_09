7c4131194adb83b995514deea8c2d501412fb3aa
T x <emraslanovsupan290@gmail.com>
2023-10-09 20:24:46 +0400

    Task completed

---
+++ Task09_10_23/Task09_10_23/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task09_10_23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            restart:
+            Console.WriteLine("Metni daxil edin:");
+            string input = Console.ReadLine().ToLower().Trim();
+            string[] words = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string result = "";
+            if (input.Length <= 0)
+            {
+                Console.WriteLine("Sehv input daxil edibsiz");
+                goto restart;
+            }
+            foreach (string word in words)
+            {
+                result += Capitalize(word) + " ";
+            }
+            Console.WriteLine(result);
+        }
+        static string Capitalize(string word)
+        {
+            char[] chars = word.ToLower().ToCharArray();
+            chars[0] = char.ToUpper(chars[0]);
+
+            return new string(chars);
+        }
+    }
+}
